47b8571c653f72b32405bec84f8738223c245085
erkus-circus <43392660+erkus-circus@users.noreply.github.com>
2018-10-28 17:47:52 -0400

    e

--- google/google.py
+++ google/google.py
@@ -1,9 +1,8 @@
 import webbrowser as wbb
-from urllib import quote_plus
 
 words = command.split()
 
 if len(words) < 2:
     words = [1,'']
 
-wbb.get().open_new_tab('https://www.google.com/search?q=' + quote_plus(words[1:]) + '&ie=UTF-8')
+wbb.get().open_new_tab('https://www.google.com/search?q=' + words[1:])
