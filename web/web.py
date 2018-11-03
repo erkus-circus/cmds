4ae9ee74ccbbd249652f838ef9470e9f8b7b9190
erkus-circus <43392660+erkus-circus@users.noreply.github.com>
2018-11-03 14:43:30 -0400

    Fixid web.py error message.

--- web/web.py
+++ web/web.py
@@ -2,5 +2,5 @@ import webbrowser as wbb
 
 if len(command.split()) < 2:
     print('Usage: web <URL>')
-
-wbb.get().open_new_tab(command.split()[1])
+else:
+    wbb.get().open_new_tab(command.split()[1])
